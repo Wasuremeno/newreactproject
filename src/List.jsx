d007a96fd4e0ef6d5a256bacc2f28cb8cafc16fa
Wasuremeno <wasuremono19@gmail.com>
2024-08-09 18:00:09 +0300

    react app

---
+++ src/List.jsx
@@ -0,0 +1,8 @@
+function List(){
+const fruits = ["apple", "orange", "banana", "coconut", "pineapple"]
+
+const listItems = fruits.map(fruit => <li>{fruit}</li>);
+
+} 
+return(fruits)
+export default List 
